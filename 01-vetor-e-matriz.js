0ebcc9da580f70252674dff9d3c0760ab56c68be
mateussfortes <mateussilveirafortes@gmail.com>
2024-02-05 22:01:15 -0300

    Vetor e matriz

---
+++ 01-vetor-e-matriz.js
@@ -0,0 +1,17 @@
+var m1 = [
+    ["G", "R", "Ê"],
+    ["M", "I", "O"]
+];
+
+var v1 = ["G", "R", "Ê", "M", "I", "0"];
+
+// ALTERAR UM ELEMENTO DA MATRIZ
+m1[1][1] = "F";
+
+//REMOVER UM ELEMENTO DO VETOR
+v1.splice(0,1);
+
+
+
+console.log(m1);
+console.log(v1);
